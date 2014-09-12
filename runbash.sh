9e6eacf6dee9d0a221c63fb670beb04fb1f2d366
andre <goroh@hotmail.com>
2014-09-12 12:57:20 -0300

    shell fix

--- runbash.sh
+++ runbash.sh
@@ -2,9 +2,14 @@ ENVBASH=$1
 ENVBASH=${ENVBASH:-"bash"}
 # para ter acesso ao Docker no host
 GID=$(getent group docker | cut -d: -f3)
-docker run --rm -t -i -p 4200:4200 \
+SIAPORT=${SIAPORT:-80}
+USERPWD=
+echo "Will use $SIAPORT port"
+
+docker run --rm -t -i -p $SIAPORT:4200 \
    -v /var/run/docker.sock:/var/run/docker.sock \
    -v /usr/bin/docker:/usr/bin/docker \
    -e GID=$GID \
+   -e USERPWD=$USERPWD \
    andrefernandes/docker-shellinabox:latest $ENVBASH ${@:2}
 
